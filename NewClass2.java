95afd10ab50d93f8035d1cda5dd78e53c6922f04
miorn <37542496+miorn@users.noreply.github.com>
2018-04-16 15:20:14 +0300

    initial variable

--- NewClass2.java
+++ NewClass2.java
@@ -1,8 +1,7 @@
-import java.lang.Scanner; 
 publis class NewClass2{
 	public static void main(String[] args){
 		System.out.println("Fucking new class111!!!")
-		Scanner scanner = new Scanner(System.in);
-		
+		//Scanner scanner = new Scanner(System.in);
+		int initialNumber = 5;
 	}
 }
